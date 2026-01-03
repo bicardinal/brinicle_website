4435e8cb6602c2b12706b158a7795fed1c8491ab
Mohammad jafari <mohammad.jaf013@gmail.com>
2026-01-04 02:18:35 +0330

    brinicle web site

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,6 @@ const nextConfig: NextConfig = {
     maxInactiveAge: 25 * 1000,
     pagesBufferLength: 2,
   },
-  // Enable instrumentation to suppress performance errors
-  experimental: {
-    instrumentationHook: true,
-  },
 };
 
 export default nextConfig;
